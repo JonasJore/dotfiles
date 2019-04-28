09c3df14e15d15cdccf64403c0a13f50b4030d84
JonasJore <jonas.jore@gmail.com>
2019-04-28 22:32:03 +0200

    install.sh actually does something now (macos only) :coffee:

--- install.sh
+++ install.sh
@@ -2,17 +2,87 @@
 
 #TODO: see if possible to automate installation of oh-my-zsh
 
+#variables:
+CURRENT_DIR="$(cd "$(dirname "$0")"; pwd)"
+HOMEBREW_REPO="https://raw.githubusercontent.com/Homebrew/install/master/install"
+PACKAGES=(
+  wget
+  node
+  git
+  git-crypt
+  npm
+  python3
+  go
+  postgresql
+  docker
+  heroku
+)
+
+CASKS=(
+  iterm2
+  firefox
+  dropbox
+  google-drive
+  libreoffice
+  slack
+  vlc
+  visual-studio-code
+  intellij-idea
+  spotify
+  alfred
+)
+
+FONTS=(
+  font-inconsolidata
+  font-roboto
+  font-clear-sans
+)
+
 if [ "$(uname)" == "Darwin" ]; then
   #install brew
   if [ "$(which brew)" == "/usr/local/bin/brew" ]; then
     echo "brew installed"
   else 
-    echo "brew not installed"
+    echo "install Homebrew? [Y/n]"
+    read HOMEBREW_INSTALLED
+    if [ $HOMEBREW_INSTALLED == "Y" ]; then
+      echo "installing Homebrew..."
+      ruby -e "$(curl -fsSL $HOMEBREW_REPO)"
+    fi
   fi
-  #call separate script with all packages to install with brew
+  #updates homebrew recipes
+  brew update
+
+  brew tap caskroom/cask
+  brew tap caskroom/versions
+  
+  #installs all needed brew packages
+  echo "installing packages..."
+  brew install ${PACKAGES[@]}
+
+  echo "installing cask apps..."
+  brew install caskroom/cask/brew-cask
+  brew cask install ${CASKS[@]}
+
+  echo "installing fonts..."
+  brew cask install ${FONTS[@]}
+
+  echo "cleaning up..."
+  brew cleanup --force
+
+  echo "configuring macOS..."  
+  #fast key repeat rate
+  defaults write NSGlobalDomain KeyRepeat -int 0
+
 else
   #everything that will happen if not run a mac (later functionality
   echo "not a mac"
 fi
 
+#installs npm globally
+npm install -g npm
+
 #symlink all dotfiles to ~
+ln -sv $CURRENT_DIR/.zshrc ~
+ln -sv $CURRENT_DIR/.vimrc ~
+
