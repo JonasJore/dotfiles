8592d4415293ddb50a7cd041dc66087144293a17
JonasJore <jonas.jore@gmail.com>
2023-01-15 22:44:38 +0100

    removed useless aliases

--- zsh_aliases.sh
+++ zsh_aliases.sh
@@ -2,10 +2,10 @@
 ### CUSTOM ALIASES ###
 ######################
 
-alias zshconfig="vim ~/.zshrc"
+alias zshc="vim ~/.zshrc"
 alias sourcezsh="source ~/.zshrc"
 alias ohmyzsh="vim ~/.oh-my-zsh"
-alias bashconfig="vim ~/.bash_profile"
+alias bashc="vim ~/.bash_profile"
 alias sourcebash="source ~/.bash_profile"
 # convenvience
 alias home="cd ~"
@@ -14,23 +14,17 @@ alias h="history"
 alias rm="rm -i"
 alias brewery="brew update && brew upgrade && brew cleanup"
 alias python="python3"
+# maven
 alias mci="mvn clean install"
 alias mcifast="mvn clean install -DskipTests"
-# python
-alias pip="pip3"
-alias python="python3"
 # npm
 alias nrun="npm run dev"
 alias nins="npm install"
 alias nbld="npm run build"
-# killswitches
-alias killfirefox="pkill -9 firefox"
 # wttr
 alias wttr="curl -4 http://www.wttr.in/Oslo"
 alias wttrHome="curl -4 http://www.wttr.in/Rjukan"
 alias wttrKongs="curl -4 http://www.wttr.in/Kongsberg"
 alias moon="curl -4 http://www.wttr.in/Moon"
-# etc
-alias composer="php /usr/local/bin/composer"
 # java
 alias javaLocation="/usr/libexec/java_home"
