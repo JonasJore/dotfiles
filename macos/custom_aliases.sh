40f0d426dd7349e4c0c594c3430d6fd107122bc6
Jonas <15245356+JonasJore@users.noreply.github.com>
2024-04-14 20:21:11 +0200

    ✨ Added alias for updating and downloading new homebrew packages
    Inspired by what Marvels thor says when he wants another glass of beer 🍺

--- macos/custom_aliases.sh
+++ macos/custom_aliases.sh
@@ -9,16 +9,12 @@ alias ohmyzsh="vim ~/.oh-my-zsh"
 alias home="cd ~"
 alias c="clear"
 alias h="history"
-alias rm="rm -i"
 alias brewery="brew update && brew upgrade && brew cleanup"
 alias python="python3"
+alias another="brew update && brew upgrade"
 # maven
 alias mci="mvn clean install"
 alias mcifast="mvn clean install -DskipTests"
-# npm
-alias nrun="npm run dev"
-alias nins="npm install"
-alias nbld="npm run build"
 # wttr
 alias wttr="curl -4 http://www.wttr.in/Oslo"
 alias wttrHome="curl -4 http://www.wttr.in/Rjukan"
