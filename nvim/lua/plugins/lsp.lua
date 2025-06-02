95f98618898d375c38d0c3a06d34fd84aab27af4
Jonas Jore <jonas.jore@gmail.com>
2025-06-02 19:33:43 +0200

    nvim lsp config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -2,9 +2,18 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     servers = {
+      -- lua
       lua_ls = {},
+      -- typescript
       tsserver = {},
       eslint = {},
+      -- python
+      pyright = {},
+      -- rust
+      rust_analyzer = {},
+      -- go
+      gopls = {},
+      tailwindcss = {},
     },
   },
 }
